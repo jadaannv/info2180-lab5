b8d5323b202e955d87246b900c82f3dbee99547f
Jada-Ann Vite <jadaannv@gmail.com>
2023-11-19 14:02:23 -0500

    Completed Exercise 5

--- world.js
+++ world.js
@@ -1,5 +1,6 @@
 function labStarter(){
     let countrybtn = document.getElementById("lookup");
+    let citybtn = document.getElementById("citieslookup");
 
     countrybtn.addEventListener("click", function(e){
         e.preventDefault();
@@ -23,7 +24,34 @@ function labStarter(){
         };
         httprequest.open("GET", getrequest, true);
         httprequest.send();
+        
     });
+
+    citybtn.addEventListener("click", function(e){
+        e.preventDefault();
+
+        var lookupCountry = document.getElementById("country").value;
+        var cities = document.getElementById("country").value;
+        var httprequest = new XMLHttpRequest();
+        var getrequest = ("world.php?country=" + lookupCountry + "&context=cities");
+        
+        httprequest.onreadystatechange = function(){
+            if (httprequest.readyState == XMLHttpRequest.DONE){
+                if(httprequest.status == 200){
+                    var cityInfo = httprequest.responseText;
+                    var resultdiv = document.getElementById("result");
+                    resultdiv.innerHTML = cityInfo;
+                }
+                else{
+                    alert("An Error Occurred.")
+                }
+            }
+        };
+
+        httprequest.open("GET", getrequest, true);
+        httprequest.send();
+    })
 }
 
 window.onload = labStarter;
+
--- world.php
+++ world.php
@@ -9,34 +9,69 @@ $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $p
 $country = isset($_GET['country']) ? $_GET['country'] : '';
 $country = '%'.filter_var($country, FILTER_SANITIZE_STRING).'%';
 
-//Changed query
-$stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
+//check if the lookup query is for a city
+$context = isset($_GET['context']) ? $_GET['context'] : '';
+$context = filter_var($context, FILTER_SANITIZE_STRING);
 
+//echo $context;
+
+//Updated query to obtain either city or country data
+$cityquery = $conn->query("SELECT cities.name, cities.district, cities.population
+                          FROM cities LEFT JOIN countries ON countries.code = cities.country_code
+                          WHERE countries.name LIKE '%$country%'");
+
+$cityquery->execute();
+$cityresults = $cityquery->fetchALL(PDO::FETCH_ASSOC);
+
+
+$stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
 //Combine and execute 
 $stmt->bindParam(':country', $country, PDO::PARAM_STR);
 $stmt->execute();
-
 //Display all countries
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+
+
 //Display info in table format
 ?>
-<table>
-  <tr>
-    <th> Name </th>
-    <th> Continent </th>
-    <th> Independence </th>
-    <th> Head of State </th>
-  </tr>
+<table class = countrytable>
 
-<?php foreach ($results as $row): ?>
+  <?php if($context == "cities"): ?>
+    <tr>
+      <th> Name </th>
+      <th> District </th>
+      <th> Population</th>
+    </tr>
+
+    <?php foreach ($cityresults as $row): ?>
+
+      <tr>
+        <td><?php echo $row['name']; ?></td>
+        <td><?php echo $row['district']; ?></td>
+        <td><?php echo $row['population']; ?></td>
+      </tr>
+
+    <?php endforeach; ?>
   
-  <tr>
-    <td><?php echo $row['name']; ?></td>
-    <td><?php echo $row['continent']; ?></td>
-    <td><?php echo $row['independence_year']; ?></td>
-    <td><?php echo $row['head_of_state']; ?></td>
-  </tr>
+  <?php else: ?>
+    <tr>
+      <th> Name </th>
+      <th> Continent </th>
+      <th> Independence </th>
+      <th> Head of State </th>
+    </tr>
 
-<?php endforeach; ?>
+    <?php foreach ($results as $row): ?>
+
+      <tr>
+        <td><?php echo $row['name']; ?></td>
+        <td><?php echo $row['continent']; ?></td>
+        <td><?php echo $row['independence_year']; ?></td>
+        <td><?php echo $row['head_of_state']; ?></td>
+      </tr>
+
+    <?php endforeach; ?>
+  <?php endif; ?>
+
 </table>
